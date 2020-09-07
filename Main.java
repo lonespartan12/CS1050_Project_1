08edd851b67deb6ba2ded278758ea5961c4db435
Duke Victory <lonespartan12@hotmail.com>
2020-09-07 05:29:23 +0000

    Project 1 program 1 Numbers

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+
+    // promp user
+    System.out.println("Enter first integer number: ");
+    int firstNumber = scan.nextInt();
+    System.out.println("Enter second integer number: ");
+    int secondNumber = scan.nextInt();
+
+    // display users inputs and calculate output
+    System.out.println("Your numbers are: " + firstNumber + " and " + secondNumber + ".");
+    int sum = firstNumber + secondNumber;
+    int product = firstNumber * secondNumber;
+    // sum output
+    System.out.println("Sum = " + sum);
+    // product output
+    System.out.println("Product = " + product);
   }
 }
